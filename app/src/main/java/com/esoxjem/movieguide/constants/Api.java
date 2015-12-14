14e8107b8edacc400e2c1785ff14084ad9fbdb6d
Arun Sasidharan <arunsasidharan121@gmail.com>
2015-12-14 08:08:20 +0530

    Updated backdrop image path

--- app/src/main/java/com/esoxjem/movieguide/constants/Api.java
+++ app/src/main/java/com/esoxjem/movieguide/constants/Api.java
@@ -5,7 +5,8 @@ package com.esoxjem.movieguide.constants;
  */
 public class Api
 {
-    public static final String API_KEY = "ed9ef89a3e661d070f16fce1a61b5e7";
+    public static final String API_KEY = "Enter API key here";
     public static final String GET_POPULAR_MOVIES = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=" + API_KEY;
     public static final String POSTER_PATH = "http://image.tmdb.org/t/p/w342";
+    public static final String BACKDROP_PATH = "http://image.tmdb.org/t/p/w780";
 }
