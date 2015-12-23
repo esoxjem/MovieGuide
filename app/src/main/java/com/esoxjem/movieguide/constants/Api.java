1894bb9ad464524513d5a11c86c1c3f0f764cb25
Arun Sasidharan <arunsasidharan121@gmail.com>
2015-12-23 12:33:13 +0530

    key update

--- app/src/main/java/com/esoxjem/movieguide/constants/Api.java
+++ app/src/main/java/com/esoxjem/movieguide/constants/Api.java
@@ -5,6 +5,7 @@ package com.esoxjem.movieguide.constants;
  */
 public class Api
 {
+    public static final String API_KEY = "enter API key here";
     public static final String GET_POPULAR_MOVIES = "http://api.themoviedb.org/3/discover/movie?language=en&sort_by=popularity.desc&api_key=" + API_KEY;
     public static final String GET_HIGHEST_RATED_MOVIES = "http://api.themoviedb.org/3/discover/movie?vote_count.gte=500&language=en&sort_by=vote_average.desc&api_key=" + API_KEY;;
     public static final String POSTER_PATH = "http://image.tmdb.org/t/p/w342";
